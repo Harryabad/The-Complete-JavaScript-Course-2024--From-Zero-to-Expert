b031528b6a7e697c6ce5598c29c2d45487660570
harryabad94@gmail.com <harryabad94@gmail.com>
2024-05-17 16:10:10 +0100

    Learning REST from 107

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,18 +12,6 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
-
-  orderDelivery: function({starterIndex = 1, mainIndex = 0, time = '20:00', address}){
-    console.log(`Order receiverd: ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
-  },
-
-  oderPasta: function(ing1, ing2, ing3){
-    console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`)
-  },
-
   openingHours: {
     thu: {
       open: 12,
@@ -38,7 +26,70 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function({starterIndex = 1, mainIndex = 0, time = '20:00', address}){
+    console.log(`Order receiverd: ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+  },
+
+  oderPasta: function(ing1, ing2, ing3){
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`)
+  },
+
+  orderPizza: function(mainIngredient, ...otherIngredients){
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+
 };
+
+// Destructuring
+
+// SPREAD, because on righthand side of assignment operator
+const arr = [1,2, ...[3,4]];
+
+// REST, because on lefthand side of equals sign
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+
+console.log(a,b, others);
+
+// the rest element myst be that last element
+const [pizza, , risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu]
+
+console.log(pizza, risotto, otherFood);
+
+// Objects
+const {sat, ...weekdays} = restaurant.openingHours;
+console.log(weekdays);
+
+// 2) functions
+const add = function(...numbers) {
+  console.log(numbers);
+  let sum = 0;
+  for(let i = 0; i<numbers.length; i++){
+    sum += numbers[i];
+  }
+  console.log(sum)
+};
+
+// REST syntax packs them into an array
+add(2,3);
+add(5,3,7,2);
+add(8,2,5,3,2,1,4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('Mushrooms', 'Olives', 'Onions', 'Pepper');
+restaurant.orderPizza('Mushrooms');
+
+
+/*
+//////////////////
+// The Spread Operator (...)
 
 const arr = [7, 8, 9];
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
@@ -76,7 +127,7 @@ restaurantCopy.name = 'Ristorante Roma';
 console.log(restaurantCopy.name);
 console.log(restaurant.name);
 
-
+*/
 
 
 
